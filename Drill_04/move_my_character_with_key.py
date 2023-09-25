9ea79f7545218c2022368fb86bd5b10c739aaaf2
D4LGONA(samsung) <laff3y2297@gmail.com>
2023-09-26 00:01:21 +0900

    up까지 만들었어요

--- Drill_04/move_my_character_with_key.py
+++ Drill_04/move_my_character_with_key.py
@@ -7,42 +7,88 @@ character = load_image('sprite.png')
 
 
 def handle_events():
-    global running
-    global dir
+    global running, dirX, dirY, state, frameX
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
         elif event.type == SDL_KEYDOWN:
             if event.key == SDLK_RIGHT:
-                dir += 1 # 이게 정말 쩌는 군
+                dirX += 1
             elif event.key == SDLK_LEFT:
-                dir -= 1 # 생각해 본 적이 없어
+                dirX -= 1
+            elif event.key == SDLK_UP:
+                dirY += 1
+            elif event.key == SDLK_DOWN:
+                dirY -= 1
             elif event.key == SDLK_ESCAPE:
                 running = False
         elif event.type == SDL_KEYUP:
             if event.key == SDLK_RIGHT:
-                dir -= 1
+                dirX -= 1
             elif event.key  == SDLK_LEFT:
-                dir += 1
+                dirX += 1
+            elif event.key == SDLK_UP:
+                dirY -= 1
+            elif event.key == SDLK_DOWN:
+                dirY += 1
 
-
-dir = 0
+state = 'idle'
+dirX = 0
+dirY = 0
 running = True
 frameX = 0
 frameY = 0
 x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
-
+count = 0
 
 while running:
     clear_canvas()
 
+    if dirX == 1:
+        if dirY != 0:
+            state = 'dizzy'
+            count = 0
+        else:
+            state = 'right'
+    elif dirX == -1:
+        if dirY != 0:
+            state = 'dizzy'
+            count = 0
+        else:
+            state = 'left'
+    elif dirY == 1:
+        if dirX != 0:
+            state = 'dizzy'
+            count = 0
+        else:
+            state = 'up'
+    elif dirY == -1:
+        if dirX != 0:
+            state = 'dizzy'
+            count = 0
+        else:
+            state = 'down'
+    else:
+        state = 'idle'
+
     tuk_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
-    character.clip_draw(frameX * 80, 355, 80, 43, x, y)
+    if state == 'idle':
+        character.clip_draw(frameX * 80, 356, 80, 42, x, y)
+    elif state == 'right':
+        character.clip_draw(frameX * 108, 356 - 42, 104, 42, x, y)
+    elif state == 'left':
+        character.clip_draw(frameX * 108, 356 - 42, 104, 42, x, y)
+    elif state == 'up':
+        character.clip_draw(frameX * 50, 356 - 42 - 73, 50, 72, x, y)
+
 
     update_canvas()
     handle_events()
     frameX = (frameX + 1) % 4
+    if state != 'dizzy':
+        x += dirX * 5
+        y += dirY * 5
     delay(0.1)
 
 close_canvas()
