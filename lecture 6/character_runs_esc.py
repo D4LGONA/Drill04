d97dafde34594e09a143ea5c2d7dcbb6307c2365
D4LGONA(samsung) <laff3y2297@gmail.com>
2023-09-25 11:53:31 +0900

    move_character_with_key

--- lecture 6/character_runs_esc.py
+++ lecture 6/character_runs_esc.py
@@ -7,11 +7,11 @@ character = load_image('animation_sheet.png')
 running = True
 
 def handle_events():
-    global running
+    global running # 이녀석을 지우면 esc가 작동하지 않음. 왜냐? 밖에 있는 running을 갖다 쓰지 않고 지역에서 running을 새로 만들기 때문
 
-    events = get_events()
-    for event in events:
-        if event.type == SDL_QUIT:
+    events = get_events() # 이벤트들이 담긴 리스트가 넘어옴.
+    for event in events: # 이벤트를 하나씩 꺼내서 확인.
+        if event.type == SDL_QUIT: # sdl 라이브러리에 키 코드가 정해져 있음, 얘는 윈도우 종료
             running = False
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
@@ -30,6 +30,5 @@ for x in range(0, 800, 5):
 
     frame = (frame + 1) % 8
     delay(0.05)
-
 
 close_canvas()
